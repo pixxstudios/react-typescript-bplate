6b94fede85583ed815a1c32a2d14e0bd6ec8f4d2
Gagandeep Singh <gagan.ddun@gmail.com>
2019-06-12 12:13:13 +1000

    React setup with HelloWorld component

---
+++ index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import HelloWorld from './src/components/HelloWorld';
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.querySelector('#root')
+)
